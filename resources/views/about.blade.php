79a6101672ecd8c1beacf63a5b65707d47cb9200
Jicin Thekkekara <jt@dc.ag>
2024-12-13 15:09:00 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:heading>
+        About
+    </x-slot:heading>
+    Hello from about</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout><x-slot:heading>
+        Home
+    </x-slot:heading>
+    <h1>hello from home</h1>
+</x-layout>
